24e0c321f43afbb3dc4f6d254f512e4909980c58
bhuynh-grc <huynh.bao@student.greenriver.edu>
2023-01-24 17:07:08 -0500

    added image lunch.html breakfast.html

--- index.php
+++ index.php
@@ -14,11 +14,29 @@ $f3 = Base::instance();
 
 //Define a default route ("homepage" for hello project)
 $f3->route('GET /', function (){
+
+    //Instantiate a view
     $view = new Template();
     echo $view->render("views/diner-home.html");
-
-
 });
+
+//Define a default route (328/diner/breakfast)
+$f3->route('GET /breakfast', function (){
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/breakfast.html");
+});
+
+//Define a default route (328/diner/lunch)
+$f3->route('GET /lunch', function (){
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/lunch.html");
+});
+
+
 //Run Fat-Free
 $f3->run();
 //Java -> f3.run();
