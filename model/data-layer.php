1ddd15c91a53a2212d2311591feb7589efae1beb
bhuynh-grc <huynh.bao@student.greenriver.edu>
2023-02-23 17:25:14 -0500

    add model

--- model/data-layer.php
+++ model/data-layer.php
@@ -1,8 +1,8 @@
 <?php
     function getMeal(){
-        return array("breakfast", "lunch", "diner", "dessert");
+        return array("breakfast", "lunch", "diner", "dessert", "brunch");
     }
 
     function getCondiments(){
-        return array("ketchup", "mustard", "sriracha", "hot sauce");
+        return array("ketchup", "mustard", "sriracha", "hot sauce", "pho");
     }
